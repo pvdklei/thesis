d0e19d4cc7b8bfebd311a23bc72ac75053a29a99
pvdklei <pepijnvanderklei@gmail.com>
2021-06-24 14:34:47 +0200

    cjds

--- visual.py
+++ visual.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sb
 
 def main():
     data = pd.read_csv("results_prepped.csv")
-    data = data.set_index("id")
-    print(data)
-    plt.figure()
-    data["average_drawtime"].plot.bar()
+    data4 = data[data["p"] == 0.4]
+    data8 = data[data["p"] == 0.8]
+    ax = sb.barplot(data=data.sort_values("average_drawtime"), 
+                     y="id", x="average_drawtime", hue="p")
+    ax.set(ylabel=None)
+    ax.set(xlabel="Average Drawtime (ms)")
+    ax.legend(title="Vertices per fragment")
     plt.show()
 
 if __name__ == "__main__":
